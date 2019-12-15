8e5505dd1c6dc7ef27f305de13f964efb7d0dbb0
Dancanoduor <dancanoduor1990@gmail>
2019-12-15 16:34:48 +0300

    add modify procfile

--- galleryapp/urls.py
+++ galleryapp/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.views.generic import RedirectView
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('gallery.urls')),
-    url(r'^favicon\.ico$',RedirectView.as_view(url='/static/images/favicon.ico')),
 ]
