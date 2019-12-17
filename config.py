9274f8088aa8008783254a1a66e2a87ae032957f
Dancanoduor <dancanoduor1990@gmail>
2019-12-17 14:42:57 +0300

    add modify url

---
+++ config.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'test',
+        'USER':'moringa',
+        'PASSWORD':'5555',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
